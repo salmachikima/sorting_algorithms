203239f102d34071951ffe7d37927c8d744ee9c1
salma <salmamarokti888@gmail.com>
2024-11-02 17:15:43 -0700

    1111

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,10 +1,12 @@
 #include "sort.h"
+
 /**
  * insertion_sort_list - function that sorts a doubly linked list
  * using insertion sort methode
  * @list: pointer to the head of the list
  * Return: nothing
  **/
+
 void insertion_sort_list(listint_t **list)
 {
 	int f; /*which means flag but i decided to call it f cs im lazy*/
@@ -27,13 +29,13 @@ void insertion_sort_list(listint_t **list)
 					aux1->next = aux2->next;
 					if (aux2->next)
 						aux2->next->prev = aux1;
-					if (aux1)
-						aux1->prev = aux2;
 					if (aux2)
 					{
 						aux2->prev = aux1->prev;
 						aux2->next = aux1;
 					}
+					if (aux1)
+						aux1->prev = aux2;
 					if (aux2->prev)
 						aux2->prev->next = aux2;
 					head_tmp2 = aux2->prev;
